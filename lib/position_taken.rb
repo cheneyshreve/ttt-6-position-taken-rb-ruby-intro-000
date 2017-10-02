198aa9ecb3704ec22d571ea43d446ecb308d291f
Cheney Shreve <cheney.shreve@gmail.com>
2017-10-02 23:44:42 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,9 @@
 def position_taken?(board,index)
  if board[index] == " " || board[index] == ""
    return false
- elsif board[index].include?("X") || board[index].include?("O")
+ elsif board[index].include?("X") == true || board[index].include?("O") == true
+   return true
+ elsif board[index].include?("nil") == true
    return true
  end
 
