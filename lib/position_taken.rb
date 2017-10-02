b4fe44b54d4cfa607bb159a7a1bdaa721ae90221
Cheney Shreve <cheney.shreve@gmail.com>
2017-10-02 23:43:06 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,9 @@
 # code your #position_taken? method here!
-def position_taken?
+def position_taken?(board,index)
+ if board[index] == " " || board[index] == ""
+   return false
+ elsif board[index].include?("X") || board[index].include?("O")
+   return true
+ end
 
 end
-  
